db3ecdcc5f62e28b178f1e95e046c48edec6f335
tarcisin <tarepic@gmail.com>
2022-08-20 19:21:40 -0300

    first commit

---
+++ estruturaDeDados1.py
@@ -0,0 +1,21 @@
+import random
+print("linguagem da cobrinha devradora de memória")
+
+def main():
+
+  smaller = int(input("Enter the smaller number: "))
+  larger = int(input("Enter the larger number: "))
+  myNumber = random.randint(smaller, larger)
+  count = 0
+  while True:
+    count+= 1
+    userNumber = int(input("Enter your guess: "))
+    if userNumber < myNumber:
+        print("Too small")
+    if userNumber > myNumber:
+        print("Too large")
+    else:
+        print("You got in", count, "tries!")
+        break
+if __name__ == "__main__":
+  main()
