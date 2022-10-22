421868477fb61c12deccdeb1e21eda4215fe16ac
tarcisin <tarepic@gmail.com>
2022-10-22 15:19:44 -0300

    att

--- pilha encadeada/pilha sem encadeamento.py
+++ pilha encadeada/pilha sem encadeamento.py
@@ -1,23 +1,43 @@
-class stack(object):
+class stackList(object):
+  _size = 0
   stack = []
 
   def push(self, value):
-    stack.append(value)
+    self.stack.append(value)
   
   def pop(self):
-    p = stack[-len(stack)]
-    stack.remove(-len(stack))
+    p = self.stack[self._size - 1]
+    self.stack.remove(self._size - 1)
     return p
 
   def __len__(self):
-    return self.len(stack)
+    return self.stack[self._size]
 
   def peek(self):
-    return stack[-len(stack)]
+    return self.stack[self._size - 1]
   
   def contains(self, value):
-    for i in range(len(stack)):
-      if stack[i] == value:
+    for i in range(self._size):
+      if self.stack[i] == value:
         return True
     else:
       return False
+    
+  def __repr__(self):
+    r = ""
+    for i in range(len(self.stack)):
+      r = r + str(self.stack[i]) + "\n"
+    return r
+  
+  def __str__(self):
+    return self.__repr__()
+
+fodase = stackList()
+
+fodase.push(3)
+fodase.push(2)
+fodase.push(1)
+
+print(fodase.peek)
+
+print(fodase.pop)
